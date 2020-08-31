274f13ae806776605c2312313023d125c10996e9
Marnix van Valen <marnix@alanta.nl>
2020-08-31 18:23:47 +0200

    Regenerated models

--- Kontent.Statiq.Tests/Models/CustomTypeProvider.cs
+++ Kontent.Statiq.Tests/Models/CustomTypeProvider.cs
@@ -1,7 +1,7 @@
-using Kentico.Kontent.Delivery.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Kentico.Kontent.Delivery.Abstractions;
 
 namespace Kontent.Statiq.Tests.Models
 {
@@ -26,12 +26,12 @@ namespace Kontent.Statiq.Tests.Models
 
         public Type GetType(string contentType)
         {
-            return _codenames.Keys.FirstOrDefault(type => GetCodename(type).Equals(contentType) );
+            return _codenames.Keys.FirstOrDefault(type => GetCodename(type).Equals(contentType));
         }
 
         public string GetCodename(Type contentType)
         {
-            return _codenames.TryGetValue(contentType, out var codename) ? codename : "";
+            return _codenames.TryGetValue(contentType, out var codename) ? codename : null;
         }
     }
 }

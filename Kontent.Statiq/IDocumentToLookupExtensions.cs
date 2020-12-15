e9465db03b1c38fa920e1528df713d3b622854f6
Marnix van Valen <marnix@alanta.nl>
2020-12-15 20:49:58 +0100

    Added helpers created for working with taxonomy from themes

---
+++ Kontent.Statiq/IDocumentToLookupExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Kentico.Kontent.Delivery.Abstractions;
+using Statiq.Common;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kontent.Statiq
+{
+    /// <summary>
+    /// Extensions for creating lookups from document sequences.
+    /// </summary>
+    public static class IDocumentToLookupExtensions
+    {
+        /// <summary>
+        /// Creates a lookup from a sequence of documents according to a specified metadata key
+        /// that contains a sequence of Kontent taxonomy terms.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <param name="documents">The documents.</param>
+        /// <param name="keyMetadataKey">The key metadata key.</param>
+        /// <returns>A lookup.</returns>
+        public static ILookup<ITaxonomyTerm, IDocument> ToLookupManyByTaxonomy(
+            this IEnumerable<IDocument> documents,
+            string keyMetadataKey)
+        {
+            documents.ThrowIfNull(nameof(documents));
+            keyMetadataKey.ThrowIfNull(nameof(keyMetadataKey));
+
+            return documents.ToLookupMany(keyMetadataKey, new TaxonomyTermComparer());
+        }
+    }
+}
---
+++ Kontent.Statiq/TaxonomyTermComparer.cs
@@ -0,0 +1,28 @@
+﻿using Kentico.Kontent.Delivery.Abstractions;
+using System.Collections.Generic;
+
+namespace Kontent.Statiq
+{
+    /// <summary>
+    /// Comparer for Kontent taxonomy.
+    /// <para>Use this with <see cref="global::Statiq.Common.IDocumentToLookupExtensions.ToLookupMany{TKey}(IEnumerable{Statiq.Common.IDocument}, string, IEqualityComparer{TKey})"/></para>
+    /// </summary>
+    public class TaxonomyTermComparer : IEqualityComparer<ITaxonomyTerm>
+    {
+        /// <inheritdoc />
+        public bool Equals(ITaxonomyTerm? x, ITaxonomyTerm? y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Codename == y.Codename;
+        }
+
+        /// <inheritdoc />
+        public int GetHashCode(ITaxonomyTerm obj)
+        {
+            return (obj.Codename != null ? obj.Codename.GetHashCode() : 0);
+        }
+    }
+}
